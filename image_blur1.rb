f6523bb8564b616ac7ea6b6eb6da1f2c5facc2ec
Jessica Brady <jessicabrady16@gmail.com>
2019-08-26 16:35:06 -0600

    Completed Image Blur Challenge 2

--- image_blur1.rb
+++ image_blur1.rb
@@ -1,23 +1,68 @@
+# frozen_string_literal: true
+
 class Image
+  attr_reader :grid
   def initialize(grid)
-  @grid = grid
+    @grid = grid
+  end
+
+  def blur
+    result = []
+    @grid.each_with_index do |or_row, row_index|
+      result_row = []
+      or_row.each_with_index do |cell, cell_index|
+        left_index = cell_index - 1
+        right_index = cell_index + 1
+        up_index = row_index - 1
+        down_index = row_index + 1
+
+        if left_index >= 0 && or_row[left_index] == 1
+          result_row.push(1)
+        elsif right_index < or_row.length && or_row[right_index] == 1
+          result_row.push(1)
+        elsif up_index >= 0 && @grid[up_index][cell_index] == 1
+          result_row.push(1)
+        elsif down_index < @grid.length && @grid[down_index][cell_index] == 1
+          result_row.push(1)
+        else
+          result_row.push(cell)
+        end
+
+      end
+      result.push(result_row)
+    end
+    @grid = result
   end
 
   def output_image
     @grid.each do |row|
-     puts row.join ""
+      puts row.join ''
     end
   end
 end
 
+image = Image.new(
+  [
+    [0, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 0, 1],
+    [0, 1, 0, 0],
+    [1, 0, 0, 0],
+    [1, 0, 0, 0],
+  ]
+)
 
-image = Image.new([
-  [0, 0, 0, 0],
-  [0, 1, 0, 0],
-  [0, 0, 0, 1],
-  [0, 0, 0, 0]
-])
-
+image.blur
 image.output_image
 
+expected =
+[
+  [0, 1, 0, 0],
+  [1, 1, 1, 1],
+  [0, 1, 1, 1],
+  [1, 1, 1, 1],
+  [1, 1, 0, 0],
+  [1, 1, 0, 0],
+]
 
+puts "#{expected == image.grid}"
